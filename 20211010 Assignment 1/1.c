2cb4f99bbe6e447b3ee48c15ef16cc03cd69231b
resyfer <resyfer.dev@gmail.com>
2021-09-17 16:08:49 +0530

    Ass. 1 Q1

--- 20211010 Assignment 1/1.c
+++ 20211010 Assignment 1/1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+#define MAX 100
+
+int floatingNumValid(char num[])
+{
+  /**
+   * Floating number valid if there's a number after .
+  */
+  int isValidFloat = 0;
+  for (int i = 0; i < MAX - 1; i++)
+  {
+    if (num[i] == '.' && num[i + 1] != 0)
+      isValidFloat = 1;
+  }
+  return isValidFloat;
+}
+
 int main()
 {
-  printf("Hello");
+  char num[MAX] = {0};
+  scanf("%s", num);
+
+  printf("Floating number is %s\n", ((floatingNumValid(num)) ? "valid" : "invalid"));
+
   return 0;
 }
