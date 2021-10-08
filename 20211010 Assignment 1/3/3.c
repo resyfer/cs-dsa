d0e07b9f7932f9265db3838acfd0fcba0f85290b
resyfer <palsaurav.2020@gmail.com>
2021-10-09 02:39:45 +0530

    Q3 Ass. 1

--- 20211010 Assignment 1/3/3.c
+++ 20211010 Assignment 1/3/3.c
@@ -4,12 +4,20 @@ int main() {
   int n, m;
   printf("Enter n m > ");
   scanf("%d %d", &n, &m);
+  
+  int arrayOneD[n*m];
+  int arrayTwoD[n][m];
+  for(int i = 0; i<n*m; i++)
+    scanf("%d", &arrayOneD[i]);
 
-  int k;
-  printf("Enter k >");
-  scanf("%d", &k);
+  for(int i = 0; i<n; i++)
+    for(int j = 0; j<m; j++)
+      arrayTwoD[i][j] = arrayOneD[(i*m) + j];
 
-  int i = (k / m), j = (k % m);
-  printf("Element is at A[%d][%d]", i, j);
+  for(int i = 0; i<n; i++) {
+    for(int j = 0; j<m; j++)
+      printf("%d ", arrayTwoD[i][j]);
+    printf("\n");
+  }
   return 0;
 }
